79ba7c34dcb9fa3c504c397d3a1a286ab8eb1fcc
sachingupta228 <sachingupta228@yahoo.com>
2025-03-27 21:12:08 +0000

    Improve dependencies - fixing a bug

--- src/primitives/slider.ts
+++ src/primitives/slider.ts
@@ -1,13 +1,13 @@
 import SliderThumb from '../components/Slider/assets/slider.svg';
 
-export const SliderConfig = {
+export let SliderConfig = {
     sliderBackground: '#F08D32',
     sliderHeight: '10px',
     sliderWidth: '100%',
     sliderTransition: 'all ease-in-out 0.05s',
 };
 
-export const ThumbConfig = {
+export let ThumbConfig = {
     thumbWidth: '40px',
     thumbHeight: '40px',
     thumbBackground: `url(${SliderThumb}) no-repeat center`,
